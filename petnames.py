20be2d4ce6e7c7d0ed9afbd8ef0babab177e5a00
Lauren Sisson <91545568+Mommyofpotato@users.noreply.github.com>
2023-04-28 08:48:44 -0400

    Add files via upload

---
+++ petnames.py
@@ -0,0 +1,10 @@
+#Simple python project to take in and concatenate strings
+
+print("Pet Name Generator")
+
+city=input("What city did you grow up in?\n")
+
+food=input("Whats your favorite food?\n")
+
+print("Your pets name is:\n")
+print(food+city)
